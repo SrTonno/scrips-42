81ebe702e4d5cd870e48a3c45ed93da989fbdf53
Tono Villarejo Reig <tonovillarejo@gmail.com>
2023-05-16 11:54:38 +0200

    Update vagrant_move.sh

--- vagrant_move.sh
+++ vagrant_move.sh
@@ -2,5 +2,5 @@
 set -e
 rm -rf ~/.vagrant*
 mkdir -p /goinfre/$USER/.vagrant.d
-ln -s /goinfre/$USER/.vagrant.d cd~/.vagrant.d
+ln -s /goinfre/$USER/.vagrant.d ~/.vagrant.d
 vagrant plugin expunge --reinstall --force
